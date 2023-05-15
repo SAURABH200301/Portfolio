ddb580c75bca8389e69875fc6c5146c8529b0523
SAURABH200301 <saurabhsharma200301@gmail.com>
2023-05-15 19:15:19 +0530

    added new resume

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -5,7 +5,7 @@ import resume from '../image/resume.png'
 function Resume() {
     return (
         <div className={`${classes.resume} z-1`}>
-                <a className='m-1' href='https://drive.google.com/file/d/1XNxNL36OSJE7TpN1h9kHGrjgsW0QyjFd/view?usp=sharing' download target="_blank"  rel="noreferrer">
+                <a className='m-1' href='https://drive.google.com/file/d/1w51ut1FxtQ81EhmU-GtVShGAJPlyuTWW/view?usp=share_link' download target="_blank"  rel="noreferrer">
                     <img className={classes.icon} src={resume} alt='resume' />
                 </a>
         </div>
