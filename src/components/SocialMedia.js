141854836f4e3829de098aec19b3a2b639952050
SAURABH200301 <saurabhsharma200301@gmail.com>
2026-04-09 11:51:18 +0530

    fix build issue

--- src/components/SocialMedia.js
+++ src/components/SocialMedia.js
@@ -8,24 +8,24 @@ function SocialMedia() {
     return (
         <div className={`d-flex justify-content-around ${classes.social} w-100 `}>
             <Card card='p-3 '>
-                <Zoom>
-                    <a href='https://www.linkedin.com/in/saurabh-sharma-a24259121/'>
-                        <BsLinkedin style={{ fontSize: '6vw', color: 'darkblue' }} />
-                    </a>
+                {/* <Zoom> */}
+                <a href='https://www.linkedin.com/in/saurabh-sharma-a24259121/'>
+                    <BsLinkedin style={{ fontSize: '6vw', color: 'darkblue' }} />
+                </a>
 
-                    <a href='https://github.com/SAURABH200301'>
-                        <BsGithub style={{ fontSize: '6vw', color: 'black' }} />
-                    </a>
-                    <a href='https://www.instagram.com/saurabh_0820/'>
-                        <GrInstagram style={{ fontSize: '6vw' }} />
-                    </a>
-                    <a href='https://www.facebook.com/donttaketention'>
-                        <BsFacebook style={{ fontSize: '6vw', color: 'blue' }} />
-                    </a>
-                    <a href='https://twitter.com/Saurabh71428055'>
-                        <GrTwitter style={{ fontSize: '6vw' }} />
-                    </a>
-                </Zoom>
+                <a href='https://github.com/SAURABH200301'>
+                    <BsGithub style={{ fontSize: '6vw', color: 'black' }} />
+                </a>
+                <a href='https://www.instagram.com/saurabh_0820/'>
+                    <GrInstagram style={{ fontSize: '6vw' }} />
+                </a>
+                <a href='https://www.facebook.com/donttaketention'>
+                    <BsFacebook style={{ fontSize: '6vw', color: 'blue' }} />
+                </a>
+                <a href='https://twitter.com/Saurabh71428055'>
+                    <GrTwitter style={{ fontSize: '6vw' }} />
+                </a>
+                {/* </Zoom> */}
             </Card>
         </div>
     )
