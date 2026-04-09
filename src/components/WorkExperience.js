4e8f7cf33a7d94c81d9a7755bdf4862784a0995b
SAURABH200301 <saurabhsharma200301@gmail.com>
2026-04-09 11:53:37 +0530

    build fix

--- src/components/WorkExperience.js
+++ src/components/WorkExperience.js
@@ -2,7 +2,7 @@ import React from "react";
 import Card from "./UI/Card";
 import classes from "./WorkExperience.module.css";
 import WorkCard from "./UI/WorkCard";
-import LeftIn from "../Animation/LeftIn";
+// import LeftIn from "../Animation/LeftIn";
 import { WorkExperiences, ExperienceType } from "../constants/portfolio-constant";
 
 export default function WorkExperience() {
@@ -26,10 +26,10 @@ export default function WorkExperience() {
                         <div className="d-flex justify-content-start">
                           <span
                             className={`badge text-bg-dark rounded-pill mx-1 ${w.typeOfExperience === ExperienceType.FULL_TIME
-                                ? 'bg-success'
-                                : w.typeOfExperience === ExperienceType.CONTRACT
-                                  ? 'bg-primary'
-                                  : 'bg-danger'
+                              ? 'bg-success'
+                              : w.typeOfExperience === ExperienceType.CONTRACT
+                                ? 'bg-primary'
+                                : 'bg-danger'
                               } ${classes.badgeClass}`}
                           >
                             {w.typeOfExperience}
